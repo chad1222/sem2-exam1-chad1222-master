fbe13ef2de3883793b6f538ae9b6f37165ddaae9
Trump Betty <bettych122299@cvh.rusd.us>
2017-02-01 10:41:24 -0800

    finshed step 2

--- MyFile.py
+++ MyFile.py
@@ -1,1 +1,28 @@
-g
+import turtle
+trump = turtle.Turtle()
+def AddTen(n):
+    n = n + 10
+    return n
+print ()
+
+"""
+
+l = 14
+w = 20
+def DrawRectangle(trump, l, w):
+    for i in range (4):
+        trump.forward(14)
+        trump.right(90)
+        trump.forward(w)
+DrawRectangle (trump, l, w)
+
+"""
+
+def Drawploy(exam, n):
+    for i in range (12):
+
+        trump.forward(34)
+        trump.right(30)
+
+Drawploy (trump,8)
+input()
