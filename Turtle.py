e43ae285d4363c710a5a6bfbf5f1ef198d4f802e
Trump Betty <bettych122299@cvh.rusd.us>
2017-02-03 10:30:52 -0800

    part one done

---
+++ Turtle.py
@@ -0,0 +1,24 @@
+"""Create a Turtle Program that will draw a 3-dimensional cube"""
+
+import turtle
+trump = turtle.Turtle()
+for i in range (4):
+        trump.forward(150)
+        trump.right(90)
+
+trump.left(135)
+trump.forward(100)
+trump.right(135)
+trump.forward(150)
+trump.right(45)
+trump.forward(100)
+trump.right(135)
+trump.forward(150)
+trump.left(90)
+trump.forward(150)
+trump.right(135)
+trump.forward(100)
+trump.right(45)
+trump.forward(150)
+"""Import and Call the DrawRectangle(Anyturtle, l, w) function from the
+file MyFile.py"""
